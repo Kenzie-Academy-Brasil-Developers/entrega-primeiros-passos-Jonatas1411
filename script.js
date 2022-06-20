b04689ac75627d320b27700f615e68a36e3b190d
Jonatas Andrade <106789706+Jonatas1411@users.noreply.github.com>
2022-06-20 17:20:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+let Refrigerante = 11.99;
+let Macarrao = 6.99;
+let Ervilha = 6.99;
+let Arroz = 22.99;
+let Feijao = 11.89;
+const Vinho = 70.00;
+
+let quantidadeRefri = 3;
+let quantidadeMacarrao = 4;
+let quantidadeErvilha = 3;
+let quantidadeArroz = 3;
+let quantidadeFeijao = 2;
+let quantidadeVinho = 3;
+
+totalRefri = Refrigerante * quantidadeRefri
+totalMacarrão = Macarrao * quantidadeMacarrao
+totalErvilha = Ervilha * quantidadeErvilha
+totalArroz  = Arroz * quantidadeArroz
+totalFeijão =  Feijao * quantidadeFeijao
+totalVinho = Vinho * quantidadeVinho
+
+let valorTotalRefri = 35.97;
+let valorTotalMacarrao = 27.96;
+let valorTotalErvilha = 20.97;
+let valorTotalArroz = 68.97;
+let valorTotalFeijao = 23.78;
+let valorTotalVinho = 210;
+
+
+
+total = valorTotalRefri + valorTotalMacarrao + valorTotalErvilha + valorTotalArroz + valorTotalFeijao + valorTotalVinho;
+
+function verificaParImpar(numero){
+
+    if(numero % 2 == 0){
+        console.log(`${numero} é um numero par`)
+        
+        
+
+    }else{
+        console.log(`${numero} é um numero impar`)
+
+    
+    }
+
+}
+verificaParImpar(total)
+
+
+
+
+
