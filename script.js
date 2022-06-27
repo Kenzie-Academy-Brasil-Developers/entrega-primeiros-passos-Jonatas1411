c4280ddc10da943826b0a27dc0405ae5a055177b
Jonatas Andrade <106789706+Jonatas1411@users.noreply.github.com>
2022-06-27 20:55:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,107 +1,67 @@
-let Refrigerante = 11.99;
-let Macarrao = 6.99;
-let Ervilha = 6.99;
-let Arroz = 22.99;
-let Feijao = 11.89;
+const Refrigerante = 11.99;
+const Macarrao = 6.99;
+const Ervilha = 6.99;
+const Arroz = 22.99;
+const Feijao = 11.89;
 const Vinho = 70.00;
 
-let quantidadeRefri = 3;
-let quantidadeMacarrao = 4;
-let quantidadeErvilha = 3;
-let quantidadeArroz = 3;
-let quantidadeFeijao = 2;
-let quantidadeVinho = 3;
 
-totalRefri = Refrigerante * quantidadeRefri
-totalMacarrão = Macarrao * quantidadeMacarrao
-totalErvilha = Ervilha * quantidadeErvilha
-totalArroz  = Arroz * quantidadeArroz
-totalFeijão =  Feijao * quantidadeFeijao
-totalVinho = Vinho * quantidadeVinho
-
-let valorTotalRefri = 35.97;
-let valorTotalMacarrao = 27.96;
-let valorTotalErvilha = 20.97;
-let valorTotalArroz = 68.97;
-let valorTotalFeijao = 23.78;
-let valorTotalVinho = 210;
+let totalRefri = Refrigerante * 3
+let totalMacarrao = Macarrao * 4
+let totalErvilha = Ervilha * 3
+let totalArroz  = Arroz * 3
+let totalFeijao =  Feijao * 2
+let totalVinho = Vinho * 3
 
 
+let total = totalRefri + totalMacarrao + totalErvilha + totalArroz + totalFeijao + totalVinho;
 
-total = valorTotalRefri + valorTotalMacarrao + valorTotalErvilha + valorTotalArroz + valorTotalFeijao + valorTotalVinho;
 
-function verificaParImpar(numero){
+function verificaParImpar(total){
 
-    if(numero % 2 == 0){
-        console.log(`${numero} é um numero par`)
+    if(total % 2 == 0){
+        console.log(`Um pagará ${total / 2 - totalVinho} e o outro amigo pagará ${total / 2 + totalVinho}`)
         
         
 
     }else{
-        console.log(`${numero} é um numero impar`)
+        console.log(`Um amigo pagará ${total / 2} e o outro amigo pagará ${total / 2}`)
 
     
     }
-
-let precoRefrigerante = parseFloat(prompt("Digite o valor do refrigerante")) 
-let quantidadeRefri = parseInt(prompt("Quantas Unidades ?"))
-
-total = Refrigerante * quantidadeRefri 
-
-alert(`Valor total a ser pago: ${total}`)
-
-let precoMacarrao = parseFloat(prompt("Digite o valor do macarrão"))
-let quantidadeMacarrao = parseInt(prompt("Quantas Unidades ?"))
-
-total = Macarrao * quantidadeMacarrao
-
-alert (`Valor total a ser pago: ${total}`)
-
-
-
-let precoErvilha = parseFloat(prompt("Digite o valor da ervilha"))
-let quantidadeErvilha = parseInt(prompt("Quantas Unidades ?"))
-
-total = Ervilha * quantidadeErvilha
-
-alert(`Valor total a ser pago: ${total}`)
-
-let precoArroz = parseFloat(prompt("Digite o valor do arroz"))
-let quantidadeArroz = parseInt(prompt("Quantas Unidades ?"))
-
-total = Arroz * quantidadeArroz
-
-alert(`Valor total a ser pago: ${total}`)
-
-let precoFeijao = parseFloat(prompt("Digite o valor do feijão"))
-let quantidadeFeijao = parseInt(prompt("Quantas Unidades ?"))
-
-total = Feijao * quantidadeFeijao
-
-alert(`Valor total a ser pago: ${total}`)
-
-let precoVinho  = parseFloat(prompt("Digite o valor do vinho"))
-let quantidadeVinho = parseInt(prompt("Quantas Unidades ?"))
-
-total = Vinho * quantidadeVinho
-
-
-
-alert(`Valor total a ser pago: ${total}`)
-
-let valorTotal = []
-
-total = valorTotalRefri + valorTotalMacarrao + valorTotalErvilha + valorTotalArroz + valorTotalFeijao + valorTotalVinho;
-
- 
-alert (`Valor total da compra: ${total}`)
-
 
 
 }
 verificaParImpar(total)
 
+function compras (total){
+    let precoRefrigerante = parseFloat(prompt("Digite o valor do refrigerante"))
+    let quantidadeRefrigerante = parseInt(prompt("Quantas Unidades ?"))
+    let totalRefri = precoRefrigerante * quantidadeRefrigerante
+    let precoMacarrao = parseFloat(prompt("Digite o valor do macarrão"))
+    let quantidadeMacarrao = parseInt(prompt("Quantas Unidades ?"))
+    let totalMacarrao = precoMacarrao * quantidadeMacarrao
+    let precoErvilha = parseFloat(prompt("Digite o valor do Ervilha"))
+    let quantidadeErvilha = parseInt(prompt("Quantas Unidades ?"))
+    let totalErvilha = precoErvilha * quantidadeErvilha
+    let precoArroz = parseFloat(prompt("Digite o valor do arroz"))
+    let quantidadeArroz = parseInt(prompt("Quantas Unidades ?"))
+    let totalArroz = precoArroz * quantidadeArroz
+    let precoFeijao = parseFloat(prompt("Digite o valor do feijão"))
+    let quantidadeFeijao = parseInt(prompt("Quantas Unidades ?"))
+    let totalFeijao = precoFeijao * quantidadeFeijao
+    let precoVinho = parseFloat(prompt("Digite o valor do vinho"))
+    let quantidadeVinho = parseInt(prompt("Quantas Unidades ?"))
+    let totalVinho = precoVinho * quantidadeVinho
 
+    total = totalRefri + totalMacarrao + totalErvilha + totalArroz + totalFeijao + totalVinho;
+
+    alert(`Valor total a ser pago: ${total}`)
+    console.log(`Valor total a ser pago: ${total}`)
+    
+}
+
+compras(total)
 
 
 
